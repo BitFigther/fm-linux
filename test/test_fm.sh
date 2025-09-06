e50e160323a234d4beb8677424388aca92b9f217
Yuhto <matsuoyuto3@gmail.com>
2025-09-07 01:59:21 +0900

    テストスクリプトの修正: コマンドオプションを統一し、不要なコードをコメントアウト

---
+++ test/test_fm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# 準備
+echo "$PWD"
+rm -f /tmp/fm_test_base.dat
+dir=/test/
+mkdir -p "$dir"
+echo "hello" > "$dir/file1.txt"
+
+# ベースライン作成
+echo "[TEST] Create baseline"
+../fm -B "$dir"
+
+# ファイルを変更
+echo "world" > "$dir/file1.txt"
+
+# 差分検出テスト
+echo "[TEST] Detect change"
+output=$(../fm -C "$dir" )
+#echo "$output" | grep "Change detected:"
+
+# 除外パターンテスト
+#echo "ignore" > "$dir/ignore.txt"
+#echo "[TEST] Exclude pattern"
+#output2=$(../fm --check "$dir" --exclude ignore.txt --baseline-file /tmp/fm_test_base.dat)
+# echo "$output2" | grep "ignore.txt"
+
+echo "All tests passed."

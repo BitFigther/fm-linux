f8709fbd7a975226d26dc6de6b7ea072b1db3251
Yuhto <matsuoyuto3@gmail.com>
2025-09-07 02:13:42 +0900

    test_fm.sh修正

--- test/test_fm.sh
+++ test/test_fm.sh
@@ -4,26 +4,26 @@ set -e
 # 準備
 echo "$PWD"
 rm -f /tmp/fm_test_base.dat
-dir=/test/
+dir=test/data
 mkdir -p "$dir"
 echo "hello" > "$dir/file1.txt"
 
 # ベースライン作成
 echo "[TEST] Create baseline"
-../fm -B "$dir"
+../fm --baseline "$dir" --baseline-file /tmp/fm_test_base.dat
 
 # ファイルを変更
 echo "world" > "$dir/file1.txt"
 
 # 差分検出テスト
 echo "[TEST] Detect change"
-output=$(../fm -C "$dir" )
-#echo "$output" | grep "Change detected:"
+output=$(../fm --check "$dir" --baseline-file /tmp/fm_test_base.dat)
+echo "$output" | grep "Change detected:"
 
 # 除外パターンテスト
-#echo "ignore" > "$dir/ignore.txt"
-#echo "[TEST] Exclude pattern"
-#output2=$(../fm --check "$dir" --exclude ignore.txt --baseline-file /tmp/fm_test_base.dat)
-# echo "$output2" | grep "ignore.txt"
+echo "ignore" > "$dir/ignore.txt"
+echo "[TEST] Exclude pattern"
+output2=$(../fm --check "$dir" --exclude ignore.txt --baseline-file /tmp/fm_test_base.dat)
+! echo "$output2" | grep "ignore.txt"
 
 echo "All tests passed."

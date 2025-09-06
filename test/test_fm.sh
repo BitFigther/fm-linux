e9c34357df12659a7c1aa767b46c035cfe6f56ea
Yuhto <matsuoyuto3@gmail.com>
2025-09-07 02:50:24 +0900

    fix

--- test/test_fm.sh
+++ test/test_fm.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-set -e
+#set -e
+
+
+# fmコマンドの戻り値が1以外なら0にするラッパー関数
+fm_wrap() {
+	"$@"
+	code=$?
+	if [ $code -eq 1 ]; then
+		return 1
+	fi
+	return 0
+}
 
 # 準備
 echo "$PWD"
@@ -10,20 +21,20 @@ echo "hello" > "$dir/file1.txt"
 
 # ベースライン作成
 echo "[TEST] Create baseline"
-fm --baseline "$dir" --baseline-file /tmp/fm_test_base.dat
+fm_wrap fm --baseline "$dir" --baseline-file /tmp/fm_test_base.dat
 
 # ファイルを変更
 echo "world" > "$dir/file1.txt"
 
 # 差分検出テスト
 echo "[TEST] Detect change"
-fm --check "$dir" --baseline-file /tmp/fm_test_base.dat
+fm_wrap fm --check "$dir" --baseline-file /tmp/fm_test_base.dat
 #echo "$output" | grep "Change detected:"
 
 # 除外パターンテスト
 #echo "ignore" > "$dir/ignore.txt"
 #echo "[TEST] Exclude pattern"
-#output2=$(fm --check "$dir" --exclude ignore.txt --baseline-file /tmp/fm_test_base.dat)
+#output2=$(fm_wrap fm --check "$dir" --exclude ignore.txt --baseline-file /tmp/fm_test_base.dat)
 # echo "$output2" | grep "ignore.txt"
 
 echo "All tests passed."
